3899e0d09aecc15276d62c6e495f96897a064d0c
Jordan Hewitt <hewitj2@gmail.com>
2026-01-22 21:01:03 -0800

    chore(templates): remove visomaster snapshot
    Why:
    - Only core templates should live in this repo
    
    What:
    - Remove the temporary visomaster template folder

--- templates/visomaster/download_models.py
+++
@@ -1,5 +0,0 @@
-from app.helpers.downloader import download_file
-from app.processors.models_data import models_list
-
-for model_data in models_list:
-    download_file(model_data['model_name'], model_data['local_path'], model_data['hash'], model_data['url'])
